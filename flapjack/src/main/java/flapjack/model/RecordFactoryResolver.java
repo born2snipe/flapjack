102133d9dae8d38fc66993534feaeb62d20728c1
Dan Dudley <born2snipe@gmail.com>
2008-11-12 23:48:59 -0600

    updated javadoc

--- flapjack/src/main/java/flapjack/model/RecordFactoryResolver.java
+++ flapjack/src/main/java/flapjack/model/RecordFactoryResolver.java
@@ -6,5 +6,13 @@ import flapjack.layout.RecordLayout;
  * Resolves what RecordFactory should be used based on the RecordLayout
  */
 public interface RecordFactoryResolver {
+
+    /**
+     * Attempts to resolve a RecordFactory for the given RecordLayout.
+     * If no RecordFactory is found for the given RecordLayout return null.
+     *
+     * @param layout - the RecordLayout of the record currently being processed
+     * @return the RecordFactory that is used for the given RecordLayout
+     */
     RecordFactory resolve(RecordLayout layout);
 }
