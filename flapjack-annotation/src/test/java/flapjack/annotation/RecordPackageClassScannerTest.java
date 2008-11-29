bef49d458473f35cd2fee0b0079603da1521ff27
Dan Dudley <born2snipe@gmail.com>
2008-11-29 11:34:25 -0600

    gave more informatative message when package does not exist

--- flapjack-annotation/src/test/java/flapjack/annotation/RecordPackageClassScannerTest.java
+++ flapjack-annotation/src/test/java/flapjack/annotation/RecordPackageClassScannerTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2008 Dan Dudley
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package flapjack.annotation;
 
 import flapjack.test.User;
@@ -34,5 +46,17 @@ public class RecordPackageClassScannerTest extends TestCase {
         assertTrue(classes.contains(Phone.class));
         assertFalse(classes.contains(Address.class));
         assertEquals(2, classes.size());
+    }
+    
+    public void test_scan_PackageGivenDoesNotExist() {
+        RecordPackageClassScanner scanner = new RecordPackageClassScanner();
+        scanner.setPackages(Arrays.<String>asList("does.not.exist"));
+
+        try {
+            scanner.scan();
+            fail();
+        } catch (IllegalArgumentException err) {
+            assertEquals("Could not find package \"does.not.exist\"", err.getMessage());
+        }
     }
 }
