319cb524417a811ec932fb50dedafbb89fc8d171
Dan Dudley <born2snipe@gmail.com>
2008-12-10 19:43:32 -0600

    added method to find fields by bean path

--- flapjack/src/main/java/flapjack/util/ClassUtil.java
+++ flapjack/src/main/java/flapjack/util/ClassUtil.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008 Dan Dudley
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License. 
@@ -13,6 +13,7 @@
 package flapjack.util;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 
@@ -42,6 +43,20 @@ public class ClassUtil {
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static Field findField(Class clazz, String beanPath) {
+        try {
+            if (beanPath.indexOf('.') > -1) {
+                String name = beanPath.substring(0, beanPath.indexOf('.'));
+                String path = beanPath.substring(beanPath.indexOf('.') + 1);
+                return findField(clazz.getDeclaredField(name).getType(), path);
+            } else {
+                return clazz.getDeclaredField(beanPath);
+            }
+        } catch (NoSuchFieldException e) {
+            return null;
         }
     }
 }
--- flapjack/src/test/java/flapjack/util/ClassUtilTest.java
+++ flapjack/src/test/java/flapjack/util/ClassUtilTest.java
@@ -1,22 +1,47 @@
 /**
  * Copyright 2008 Dan Dudley
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License. 
  */
 package flapjack.util;
 
-import junit.framework.TestCase;
+import flapjack.layout.SimpleFieldDefinition;
 import flapjack.layout.SimpleRecordLayout;
+import junit.framework.TestCase;
+
+import java.lang.reflect.Field;
 
 
 public class ClassUtilTest extends TestCase {
+
+    public void test_findFiled_FieldNotFound_NestElement() {
+        assertNull(ClassUtil.findField(NestClass.class, "fieldDef.doesNotExist"));
+    }
+
+    public void test_findFiled_FieldFound_NestElement() {
+        Field field = ClassUtil.findField(NestClass.class, "fieldDef.position");
+
+        assertNotNull(field);
+        assertEquals("position", field.getName());
+    }
+
+    public void test_findField_FieldFound() {
+        Field field = ClassUtil.findField(SimpleFieldDefinition.class, "position");
+
+        assertNotNull(field);
+        assertEquals("position", field.getName());
+    }
+
+    public void test_findField_FieldNotFound() {
+        assertNull(ClassUtil.findField(String.class, "doesNotExist"));
+    }
 
     public void test_findDefaultContructor() {
         assertNotNull(ClassUtil.findDefaultConstructor(SimpleRecordLayout.class));
@@ -29,7 +54,7 @@ public class ClassUtilTest extends TestCase {
         assertNotNull(obj);
         assertTrue(obj instanceof SimpleRecordLayout);
     }
-    
+
     public void test_newInstance_NoDefaultConstructor() {
         try {
             ClassUtil.newInstance(NoDefaultConstructor.class);
@@ -38,8 +63,13 @@ public class ClassUtilTest extends TestCase {
             assertEquals("Cannot construct without a default constructor", err.getMessage());
         }
     }
-    
+
     private static class NoDefaultConstructor {
-        private NoDefaultConstructor(String value){}
+        private NoDefaultConstructor(String value) {
+        }
+    }
+
+    private static class NestClass {
+        private SimpleFieldDefinition fieldDef;
     }
 }
