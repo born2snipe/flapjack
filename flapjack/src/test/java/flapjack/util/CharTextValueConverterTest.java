1222ecff9fcdbd91f8d820209263e16fb8fec25c
Dan Dudley <born2snipe@gmail.com>
2009-01-20 19:04:15 -0600

    created text converters for shorts and characters

---
+++ flapjack/src/test/java/flapjack/util/CharTextValueConverterTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2008 Dan Dudley
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+package flapjack.util;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+
+public class CharTextValueConverterTest extends TestCase {
+    private CharTextValueConverter converter;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        converter = new CharTextValueConverter();
+    }
+
+    public void test_convert() {
+        assertEquals(new Character('1'), converter.convert("1".getBytes()));
+        assertNull(converter.convert("".getBytes()));
+        assertNull(converter.convert(null));
+    }
+
+    public void test_convert_MoreThanOneByteGiven() {
+        try {
+            converter.convert("12".getBytes());
+            fail();
+        } catch (IllegalArgumentException err) {
+            assertEquals("To many bytes was given length=2, was expecting a single byte", err.getMessage());
+        }
+    }
+
+    public void test_types() {
+        Class[] classes = converter.types();
+
+        assertNotNull(classes);
+        assertEquals(2, classes.length);
+        assertTrue(Arrays.equals(new Class[]{char.class, Character.class}, classes));
+    }
+
+
+}
---
+++ flapjack/src/test/java/flapjack/util/ShortTextValueConverterTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2008 Dan Dudley
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+package flapjack.util;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+
+public class ShortTextValueConverterTest extends TestCase {
+    private ShortTextValueConverter converter;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        converter = new ShortTextValueConverter();
+    }
+
+    public void test_convert() {
+        assertEquals(new Short((short) 2), converter.convert("2".getBytes()));
+        assertEquals(new Short((short) 0), converter.convert("".getBytes()));
+        assertEquals(new Short((short) 0), converter.convert(null));
+    }
+
+    public void test_types() {
+        Class[] classes = converter.types();
+
+        assertNotNull(classes);
+        assertEquals(2, classes.length);
+        assertTrue(Arrays.equals(new Class[]{short.class, Short.class}, classes));
+    }
+
+}
