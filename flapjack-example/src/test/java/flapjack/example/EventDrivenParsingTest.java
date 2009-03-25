2e3cb2f613058ec149ca25e7e19ab9f680f62d81
Dan Dudley <born2snipe@gmail.com>
2009-03-25 18:45:53 -0500

    added javadoc

---
+++ flapjack-example/src/test/java/flapjack/example/EventDrivenParsingTest.java
@@ -0,0 +1,9 @@
+package flapjack.example;
+
+import junit.framework.TestCase;
+
+
+public class EventDrivenParsingTest extends TestCase {
+    public void test() {
+    }
+}
--- flapjack/src/main/java/flapjack/parser/NotifyingParseResultFactory.java
+++ flapjack/src/main/java/flapjack/parser/NotifyingParseResultFactory.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License. 
@@ -13,26 +13,59 @@
 package flapjack.parser;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 
-
+/**
+ * Should be used when you want to be notified when a record has been parsed or a bad record has been encountered while parsing.
+ * This is a more ideal solution if you know you are going to be processing large amounts of data and you do not want
+ * to wait for the entire file/data to be parsed before processing the records.
+ */
 public class NotifyingParseResultFactory implements ParseResultFactory {
-    private static final NotifyingParseResult results = new NotifyingParseResult();
+    private final NotifyingParseResult results = new NotifyingParseResult();
 
     public ParseResult build() {
         return results;
     }
 
+    /**
+     * Register a notifier to be notified of parsing events
+     *
+     * @param notifier - the notifier to be notified
+     */
     public void addNotifier(Notifier notifier) {
         results.addNotifier(notifier);
     }
 
     public static interface Notifier {
+        /**
+         * This method is called when a record was successfully parsed.
+         *
+         * @param obj - the domain object created from the record of data
+         */
         void addRecord(Object obj);
-        void addPartialRecord(BadRecord badRecord);
+
+        /**
+         * This method is called when a record read from a file does not match lengths with the RecordLayout
+         *
+         * @param record - the record that was read that caused the problem
+         */
+        void addPartialRecord(BadRecord record);
+
+        /**
+         * This method is called when a record read from a file encoutered a ParseException
+         *
+         * @param record - the record that was read that caused the problem
+         */
         void addUnparseableRecord(BadRecord record);
+
+        /**
+         * This method is called when a record read from a file could not resolve a RecordLayout
+         *
+         * @param record - the record that was read that caused the problem
+         */
         void addUnresolvedRecord(BadRecord record);
+
     }
 
     private static class NotifyingParseResult extends ParseResult {
