d9c28535ee5dcc6524b1430307d9d28e16c6916b
Dan Dudley <born2snipe@gmail.com>
2009-03-28 16:25:22 -0500

    changed to handle unmapped fields

--- flapjack/src/main/java/flapjack/model/BeanMappedRecordFactory.java
+++ flapjack/src/main/java/flapjack/model/BeanMappedRecordFactory.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License. 
@@ -17,14 +17,19 @@ import flapjack.util.ClassUtil;
 import flapjack.util.TypeConverter;
 
 import java.lang.reflect.Field;
+import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 
 public class BeanMappedRecordFactory implements RecordFactory {
+    private static final String NO_FIELD_MAPPING = "Could not locate field mapping for field=\"{0}\"";
+
     private Class clazz;
     private TypeConverter typeConverter;
     private Map fieldMappings = new HashMap();
+    private boolean ignoreUnmappedFields;
 
     public BeanMappedRecordFactory(Class clazz, TypeConverter typeConverter) {
         this.clazz = clazz;
@@ -34,9 +39,19 @@ public class BeanMappedRecordFactory implements RecordFactory {
     public Object build(Object obj, RecordLayout recordLayout) {
         Map fields = (Map) obj;
         Object domain = createInstance();
-        String beanPath = (String) fieldMappings.get("field1");
-        Field field = ClassUtil.findField(domain, beanPath);
-        ClassUtil.setBean(domain, beanPath, typeConverter.convert(field.getType(), (byte[]) fields.get("field1")));
+        Iterator iterator = fields.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = (String) iterator.next();
+            String beanPath = (String) fieldMappings.get(key);
+            if (beanPath == null) {
+                if (!ignoreUnmappedFields) {
+                    throw new IllegalArgumentException(MessageFormat.format(NO_FIELD_MAPPING, new String[]{key}));
+                }
+            } else {
+                Field field = ClassUtil.findField(domain, beanPath);
+                ClassUtil.setBean(domain, beanPath, typeConverter.convert(field.getType(), (byte[]) fields.get("field1")));
+            }
+        }
         return domain;
     }
 
@@ -46,6 +61,11 @@ public class BeanMappedRecordFactory implements RecordFactory {
     }
 
     public void setFieldMappings(Map fieldMappings) {
+        this.fieldMappings.clear();
         this.fieldMappings.putAll(fieldMappings);
+    }
+
+    public void setIgnoreUnmappedFields(boolean ignoreUnmappedFields) {
+        this.ignoreUnmappedFields = ignoreUnmappedFields;
     }
 }
--- flapjack/src/test/java/flapjack/model/BeanMappedRecordFactoryTest.java
+++ flapjack/src/test/java/flapjack/model/BeanMappedRecordFactoryTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License. 
@@ -24,9 +24,10 @@ import java.util.Map;
 public class BeanMappedRecordFactoryTest extends TestCase {
     private BeanMappedRecordFactory factory;
     private SimpleRecordLayout layout;
+    private Map fieldMappings;
 
     public void setUp() {
-        Map fieldMappings = new HashMap();
+        fieldMappings = new HashMap();
         fieldMappings.put("field1", "name");
 
         factory = new BeanMappedRecordFactory(Dummy.class, new TypeConverter());
@@ -34,6 +35,36 @@ public class BeanMappedRecordFactoryTest extends TestCase {
 
         layout = new SimpleRecordLayout();
         layout.addFieldDefinition(new SimpleFieldDefinition("field1", 0, 1));
+    }
+
+    public void test_build_FieldMappingNotFoundForField_IgnoreUnmappedFields() {
+        factory.setFieldMappings(new HashMap());
+        factory.setIgnoreUnmappedFields(true);
+
+        Map fields = new HashMap();
+        fields.put("field1", "value".getBytes());
+
+        Object obj = factory.build(fields, layout);
+
+        assertNotNull(obj);
+        assertTrue(obj instanceof Dummy);
+
+        Dummy dummy = (Dummy) obj;
+        assertNull(dummy.name);
+    }
+
+    public void test_build_FieldMappingNotFoundForField() {
+        factory.setFieldMappings(new HashMap());
+
+        Map fields = new HashMap();
+        fields.put("field1", "value".getBytes());
+
+        try {
+            factory.build(fields, layout);
+            fail();
+        } catch (IllegalArgumentException err) {
+            assertEquals("Could not locate field mapping for field=\"field1\"", err.getMessage());
+        }
     }
 
     public void test_build_FieldOnParent() {
@@ -50,7 +81,6 @@ public class BeanMappedRecordFactoryTest extends TestCase {
     }
 
     public void test_build_FieldNested() {
-        Map fieldMappings = new HashMap();
         fieldMappings.put("field1", "nested.name");
 
         factory = new BeanMappedRecordFactory(NestedDummy.class, new TypeConverter());
