0457e983f5613bf054ae6b9568f428f73d3362c4
Dan Dudley <born2snipe@gmail.com>
2009-05-13 22:06:21 -0500

    updated the cobol field definitions to use the same regex pattern

--- flapjack-cobol/src/main/java/flapjack/cobol/layout/DecimalFieldDefinition.java
+++ flapjack-cobol/src/main/java/flapjack/cobol/layout/DecimalFieldDefinition.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License. 
@@ -15,11 +15,9 @@ package flapjack.cobol.layout;
 import flapjack.layout.SimpleFieldDefinition;
 
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class DecimalFieldDefinition extends SimpleFieldDefinition implements CobolFieldDefinition {
-    private static final Pattern MULTIPLIER_PATTERN = Pattern.compile("9\\(([0-9]+)\\)");
     private String pattern;
 
     public DecimalFieldDefinition(String name, int position, String pattern) {
